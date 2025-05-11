188f400211a0fed766d954d3b16b32a9599b0743
anonimat0 <u202114714@upc.edu.pe>
2025-05-11 15:23:34 -0500

    feat: added spacework of microservice-profile

---
+++ microservice-profile/src/test/java/com/agrocontrol/msvc_profile/MicroserviceProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrocontrol.msvc_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
