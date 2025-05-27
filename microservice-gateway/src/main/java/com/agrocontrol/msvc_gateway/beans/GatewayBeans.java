4185a603a51c21a10223578317e032e6b55dd05f
anonimat0 <u202114714@upc.edu.pe>
2025-05-27 03:16:10 -0500

    feat: add elements of security in msvc-gateway and msvc-config
    Co-Authored-By: Erick <89364442+ErickVasquezz@users.noreply.github.com>
    Co-Authored-By: Juan Jesús Calisaya Sánchez <112789857+JuanCali999@users.noreply.github.com>
    Co-Authored-By: Johan Príncipe Godoy <73038573+JP19-03@users.noreply.github.com>
    Co-Authored-By: Rodrigo Alcántara <103437322+Roiander@users.noreply.github.com>
    Co-Authored-By: Beth Oneglio <106390542+bethnlg@users.noreply.github.com>

---
+++ microservice-gateway/src/main/java/com/agrocontrol/msvc_gateway/beans/GatewayBeans.java
@@ -0,0 +1,49 @@
+package com.agrocontrol.msvc_gateway.beans;
+
+import com.agrocontrol.msvc_gateway.filters.AuthFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@AllArgsConstructor
+public class GatewayBeans {
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public AuthFilter authFilter(WebClient.Builder webClientBuilder) {
+        return new AuthFilter(webClientBuilder);
+    }
+
+    @Bean
+    @Profile(value = "oauth2")
+    public RouteLocator routeLocatorOauth2(RouteLocatorBuilder builder, AuthFilter authFilter) {
+        return builder
+                .routes()
+                .route(route -> route
+                        .path("/msvc-iam/api/v1/roles/**")
+                        .filters(filter -> filter.filter(authFilter))
+                        .uri("lb://msvc-iam")
+                )
+                .route(route -> route
+                        .path("/msvc-profile/api/v1/profiles/**")
+                        .filters(filter -> filter.filter(authFilter))
+                        .uri("lb://msvc-profile")
+                )
+                .route(route -> route
+                        .path("/msvc-iam/api/v1/authentication/**")
+                        .uri("lb://msvc-iam")
+                )
+                .build();
+    }
+}
